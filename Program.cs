b40483746558a9897d4ed98e052ee1889c95f224
IbrahimH88 <120130604+IbrahimH88@users.noreply.github.com>
2023-01-27 17:50:35 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cprg211_lab_1
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            Person person1 = new Person(1, "Ian", "Brooks", 30, "White");
+            Person person2 = new Person(2, "Gina", "James", 18, "Green", );
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Person person4 = new Person(4, "Mary", "Beals", 28, "yellow");
+
+
+            Console.WriteLine(person2.ToString());
+            Console.WriteLine(person3.ToString2());
+            Console.WriteLine(person4.ToString3());
+            Console.WriteLine(person1 + int age);
+            Console.WriteLine("The youngest person is:"+ );
+        } 
+            
+           
+        } 
+    }
+
+}
